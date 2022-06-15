7c95be8eb47ffa5ab9adc957704169c17522c1f7
emilsonrodsorus <emilson.rodriguez.freelance@gmail.com>
2022-06-14 18:34:20 -0700

    development added auth and validation of user connected

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function() {
+    return await App.initWeb3();
+  },
+
+  initWeb3: async function() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+    console.log('loading auth');
+    console.log(web3);
+    return App.initContract();
+  },
+
+  initContract: function() {
+    
+  },  
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
